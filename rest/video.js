3dc1e0ce6e0c466e566a6f9c44e48b759fd79bb7
Arthur Vivian <arthur.vivian@gmail.com>
2013-02-05 21:53:34 +0000

    removed some hard coded lists

--- rest/video.js
+++ rest/video.js
@@ -8,92 +8,13 @@ var currentVideo = null;
 var VIDEO_FOLDER = '/home/arthur/Videos'
 
 var VID_LIST = {
-	'looper': 'Looper 2012.avi',
-	'moonrise_kingdom': 'Moonrise.Kingdom.2012.LiMiTED.BRRip.XVID.AbSurdiTy.avi',
-	'safety_not_guarenteed': 'sparks-sng-xvid.avi'
 }
 
 var VID_NAME = {
-	'looper': 'Looper',
-	'moonrise_kingdom': 'Moonrise Kingdom',
-	'safety_not_guarenteed': 'Safety Not Guaranteed'
 }
 
 var VIDS = [
-{
-	'_id': 'ttss',
-	'filename': "Tinker Tailor Soldier Spy 2011 R5 DVDRip XviD AC3-26K.avi",
-	'name': 'Tinker Taylor Soldier Spy'
-},
-{
-	'_id': 'sng',
-	'filename': "sparks-sng-xvid.avi",
-	'name': 'Safety Not Guaranteed'
-},
-{
-	'_id': 'sw5',
-	'filename': "V-The.Empire.Strikes.Back[1980]DvDrip-aXXo.avi",
-	'name': 'Star Wars Ep.5 - The Empire Strikes Back'
-},
-{
-	'_id': 'looper',
-	'filename': 'Looper 2012.avi',
-	'name': 'Looper'
-},
-{
-	'_id': 'irons',
-	'filename': "Iros.Sky.2012.XviD.700MB.avi",
-	'name': 'Iron Sky'
-},
-{
-	'_id': 'brave',
-	'filename': "Brave.2012.R5.DVDRip.XViD.LiNE-UNiQUE.avi",
-	'name': 'Brave'
-},
-{
-	'_id': 'moonk',
-	'filename':"Moonrise.Kingdom.2012.LiMiTED.BRRip.XVID.AbSurdiTy.avi",
-	'name':'Moonrise Kingdom'	
-}];
-// "twiz-crazystupidlove-b.avi",
-// "sparks-sng-xvid.avi",
-// "South.Park.S16E06.REPACK.HDTV.x264-ASAP.mp4",
-// "Its.Always.Sunny.in.Philadelphia.S08E02.HDTV.x264-2HD.mp4",
-// "V-The.Empire.Strikes.Back[1980]DvDrip-aXXo.avi",
-// "Looper 2012.avi",
-// "IV-A.New.Hope[1977]DvDrip-aXXo.avi",
-// "Breaking.Bad.S05E04.Fifty-One.HDTV.x264-FQM.mp4",
-// "South.Park.S16E05.HDTV.x264-2HD.mp4",
-// "Iros.Sky.2012.XviD.700MB.avi",
-// "twiz-crazystupidlove-a.avi",
-// "SAMPLE.avi",
-// "Brave.2012.R5.DVDRip.XViD.LiNE-UNiQUE.avi",
-// "Moonrise.Kingdom.2012.LiMiTED.BRRip.XVID.AbSurdiTy.avi",
-// "deprived-mib3-xvid-cd1.avi",
-// "The.Artist.2011.720p.BRRip.x264.AAC-ViSiON.mp4",
-// "VI-Return.Of.The.Jedi[1983]DvDrip-aXXo.avi",
-// "South.Park.S16E07.HDTV.x264-COMPULSiON.mp4",
-// "Waterloo.avi",
-// "South.Park.S16E02.HDTV.x264-ASAP.mp4",
-// "Batman The Dark Knight [2008]-720p-BRrip-x264-KurdishAngel.mp4",
-// "batman.begins-phrax.mp4",
-// "Its.Always.Sunny.in.Philadelphia.S08E01.HDTV.x264-EVOLVE.mp4",
-// "Breaking.Bad.S05E02.Madrigal.HDTV.x264-FQM.mp4",
-// "South.Park.S16E03.HDTV.x264-2HD.mp4",
-// "The.Dictator.2012.UNRATED.BDRip.XviD-AMIABLE.avi",
-// "Breaking.Bad.S05E01.Live.Free.or.Die.HDTV.x264-FQM.mp4",
-// "II-Attack.Of.The.Clones[2002]DvDrip-aXXo.avi",
-// "deprived-rubysparks-xvid-cd2.avi",
-// "South.Park.S16E01.HDTV.XviD-2HD.avi",
-// "South.Park.S16E04.HDTV.x264-2HD.mp4",
-// "I-The.Phantom.Menace[1999]DvDrip-aXXo.avi",
-// "deprived-rubysparks-xvid-cd1.avi",
-// "deprived-mib3-xvid-cd2.avi",
-// "Its.Always.Sunny.in.Philadelphia.S08E03.HDTV.x264-KILLERS.mp4",
-// "III-Revenge.Of.The.Sith[2005]DvDrip-aXXo.avi",
-// "psig-tasm.2012.retail.dvdrip.xvid.avi"
-
-
+]
 
 exports.setup = function (expressApp) {
 	
